c2fa6dc52ee807b3b73023e0996924be3f45e316
Dong ki <119654357+99MotiveU@users.noreply.github.com>
2025-04-14 09:14:26 +0900

    Delete MafiaG(la) directory

--- MafiaG(la)/src/MafiaG/Main.java
+++
@@ -1,9 +0,0 @@
-package MafiaG;
-
-import javax.swing.*;
-
-public class Main {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new LoginUI().showLoginUI());
-    }
-} 
